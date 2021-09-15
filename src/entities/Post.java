84f3d1de094afb19f0199f2925191bf50d14a423
Albert Fortunato <f.ortunato@hotmail.com>
2021-09-15 11:28:46 -0300

    Aula Java

---
+++ src/entities/Post.java
@@ -0,0 +1,85 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Post {
+
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+    private Date momento;
+    private String titulo;
+    private String content;
+    private Integer likes;
+
+    private List<Comment> comments = new ArrayList<>();
+
+    public Post (){
+    }
+
+    public Post(Date momento, String titulo, String content, Integer likes) {
+        this.momento = momento;
+        this.titulo = titulo;
+        this.content = content;
+        this.likes = likes;
+    }
+
+    public Date getMomento() {
+        return momento;
+    }
+
+    public void setMomento(Date momento) {
+        this.momento = momento;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void addComment(Comment comment){
+        comments.add(comment);
+    }
+    public void removeComment(Comment comment){
+        comments.remove(comment);
+    }
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(titulo + "\n");
+        sb.append(likes);
+        sb.append(" Likes - ");
+        sb.append(sdf.format(momento)+"\n");
+        sb.append(content + "\n");
+        sb.append("Comments:\n");
+        for(Comment c : comments){
+            sb.append(c.getTexto()+ "\n");
+        }
+        return sb.toString();
+    }
+}
+
